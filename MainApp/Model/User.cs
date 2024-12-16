e2bf380659ce1d14760d3337809e23225263bab6
Ayla Wilkon <ayla.wilkon@gmail.com>
2024-12-16 14:22:18 +0100

    Models added, with classes address and user

---
+++ MainApp/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MainApp.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; } //behöver vara en sträng pga för många siffror för int
+        public Address Adress { get; set; }
+
+        [JsonConstructor]
+        private User()
+        {
+
+        }
+
+        public User( string firstName, string lastName, string email, string phoneNumber, string streetAdress, string postalCode, string city)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Adress = new Address
+            {
+                StreetName = streetAdress,
+                PostalCode = postalCode,
+                City = city,
+            };
+        }
+    }
+}
